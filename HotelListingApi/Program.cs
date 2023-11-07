daa6dec56fb0cbc3c8236cfe1344e18f81a22046
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-11-08 01:27:20 +0200

    Add project files.

---
+++ HotelListingApi/Program.cs
@@ -0,0 +1,67 @@
+namespace HotelListingApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // CORS ----------> 
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  policy =>
+                                  {
+                                      policy.AllowAnyOrigin()
+                                      .AllowAnyMethod()
+                                      .AllowAnyHeader();
+
+                                  });
+            });
+
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/echo",
+                      context => context.Response.WriteAsync("echo"))
+                                 .RequireCors(MyAllowSpecificOrigins);
+
+                endpoints.MapControllers()
+                .RequireCors(MyAllowSpecificOrigins);
+
+
+            });
+
+            app.Run();
+        }
+    }
+}
