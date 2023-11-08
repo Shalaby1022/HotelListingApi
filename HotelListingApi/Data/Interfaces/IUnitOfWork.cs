a6e3a63f994d67936c1013bf22a7767c5f8e381e
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-11-08 18:53:47 +0200

    IUnitOfWork interface to manage transactions and coordinate repository work

---
+++ HotelListingApi/Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using HotelListingApi.Models;
+
+namespace HotelListingApi.Data.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+         IGenericRepository<Country> CountriesRepository { get; }
+        IGenericRepository<Hotel> HotelsRepsitory { get; }
+        Task SaveAsync();
+    }
+}
