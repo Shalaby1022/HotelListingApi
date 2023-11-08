cf19fcc4d896a1f3f80b8d14d8db7f6e0a4d3819
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-11-08 18:52:52 +0200

    - Added IGenericRepository interface with CRUD operations

---
+++ HotelListingApi/Data/Interfaces/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace HotelListingApi.Data.Interfaces
+{
+    public interface IGenericRepository<T> where T : class 
+    {
+        Task<IEnumerable<T>> GetAllAsync(
+            Expression<Func<T, bool>> expression = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderdBy = null,
+            List<string> includes = null
+            );
+
+        Task<T> GetByIdAsync(
+            Expression<Func<T, bool>> expression, List<string> includes = null);
+
+        Task CreateAsync(T entity);
+        Task CreateRangeAsync(IEnumerable<T> enttities);
+        Task DeleteAsync(int id);
+        void DeleteRangeAsync(IEnumerable<T> enttities);
+        void UpdateAsync(T entity);
+
+
+    }
+}
