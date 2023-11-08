a420ca428a5850abf08ee91ad4a675d1860233f6
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-11-08 18:55:13 +0200

    Create AutoMapper mapping profiles for entities and DTOs

---
+++ HotelListingApi/Profiles/MappingProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using HotelListingApi.DTOs.CountryDTOs;
+using HotelListingApi.DTOs.HotelDTOs;
+using HotelListingApi.Models;
+
+namespace HotelListingApi.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Country , CountryDto>().ReverseMap();
+            CreateMap<Country , CreateCoutnryDto>().ReverseMap();
+
+
+            CreateMap<Hotel , HotelDto>().ReverseMap();
+            CreateMap<Hotel , CreateHotelDto>().ReverseMap();
+        }
+    }
+}
