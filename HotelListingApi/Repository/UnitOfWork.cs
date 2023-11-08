c875a97ac6cfaa1523fad2b5105aa9e94fb85cf8
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-11-08 18:55:27 +0200

    Implement UnitOfWork for managing repository interactions

---
+++ HotelListingApi/Repository/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using HotelListingApi.Data;
+using HotelListingApi.Data.Interfaces;
+using HotelListingApi.Models;
+
+namespace HotelListingApi.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private IGenericRepository<Country> _countryRepository;
+        private IGenericRepository<Hotel> _hotelRepository;
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IGenericRepository<Country> CountriesRepository => _countryRepository ?? new GenericRepsitory<Country>(_context);    
+
+        public IGenericRepository<Hotel> HotelsRepsitory => _hotelRepository ?? new GenericRepsitory<Hotel>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
