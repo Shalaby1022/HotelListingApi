65ef5790a0848181c591d0cf76994c6a6ea9e290
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-11-10 15:28:47 +0200

    Extend IdentityUser to include custom properties FName and LName with validation attributes.

---
+++ HotelListingApi/Models/Appuser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelListingApi.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string FName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string LName { get; set; }
+    }
+}
