fa5e0e1b6fbc95b8406b88feb42c7ed40f17647b
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-11-11 18:40:07 +0200

    Add Jwt configuration class

---
+++ HotelListingApi/Helpers/AuthJwt/Jwt.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelListingApi.Helpers.AuthJwt
+{
+    public class Jwt
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public double Durationindays { get; set; }
+
+    }
+}
