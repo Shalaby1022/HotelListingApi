e1afa83c3358b66611c67ff00e8505491d15457d
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-11-12 18:43:21 +0200

    add AddRoleDto for role assignment

---
+++ HotelListingApi/DTOs/AuthDTOs/AddRoleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelListingApi.DTOs.AuthDTOs
+{
+    public class AddRoleDto
+    {
+        public string UserId { get; set; }
+        public string RoleName { get; set; }
+
+
+    }
+}
