d68ac1b21ccb61ae63ac6b648fb5a35ecf3b7fe7
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-11-15 01:40:06 +0200

    Enhance Create and Update endpoints for Hotels with improved validation and error handling.

--- HotelListingApi/Controllers/HotelController.cs
+++ HotelListingApi/Controllers/HotelController.cs
@@ -3,6 +3,7 @@ using HotelListingApi.Data.Interfaces;
 using HotelListingApi.DTOs.CountryDTOs;
 using HotelListingApi.DTOs.HotelDTOs;
 using HotelListingApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelListingApi.Controllers
@@ -72,6 +73,82 @@ namespace HotelListingApi.Controllers
             }
         }
 
+        [Authorize(Roles ="Admin")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateNewHotel([FromBody] CreateHotelDto createHotelDto)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError($"Invalid POST attempt in {nameof(CreateHotelDto)}");
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                if (createHotelDto == null) return BadRequest(ModelState);
+                var hotelMap = _mapper.Map<Hotel>(createHotelDto);
+
+                await _unitOfWork.HotelsRepsitory.CreateAsync(hotelMap);
+                await _unitOfWork.SaveAsync();
+
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"An error occurred while creating and adding new Hotel {nameof(createHotelDto)}.");
+                return StatusCode(500, "Internal server error");
+            }
+
+
+        }
+
+
+        [HttpPut("{hotelID:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateExisitingHotel(int hotelID, [FromBody] UpdateHotelDto updateHotelDto)
+        {
+            if (updateHotelDto == null) return BadRequest(ModelState);
+
+            if (hotelID != updateHotelDto.Id) return BadRequest(ModelState);
+
+            if (!ModelState.IsValid || hotelID < 1)
+            {
+                _logger.LogError($"Invalid Put attempt in {nameof(UpdateExisitingHotel)}");
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var hotelRetrieving = await _unitOfWork.HotelsRepsitory.GetAllAsync(q => q.Id == hotelID);
+
+                if ( hotelRetrieving.FirstOrDefault() == null)
+                {
+                    _logger.LogError($"An error occurred while retreving the original data Coz id isn't matching" +
+                                                                        $" existing ones {nameof(UpdateExisitingHotel)}.");
+                    return BadRequest("id deosn't match");
+                }
+
+                var hotelMap = _mapper.Map<Hotel>(updateHotelDto);
+                _unitOfWork?.HotelsRepsitory.UpdateAsync(hotelMap);
+                await _unitOfWork.SaveAsync();
+
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"An error occurred while updating existing Hotel {nameof(UpdateExisitingHotel)}.");
+                return StatusCode(500, "Internal server error");
+            }
+
+        }
 
     }
 }
