b2572052439dd24bf778cd9c9bb0b3f420d1634a
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-11-15 01:41:13 +0200

    Add UpdateCountryDto for handling country update requests

---
+++ HotelListingApi/DTOs/HotelDTOs/UpdateHotelDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListingApi.DTOs.HotelDTOs
+{
+    public class UpdateHotelDto
+    {
+        
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name should be between 2 and 50 characters.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Address is required.")]
+        [StringLength(maximumLength:100 , ErrorMessage = "Address 100 characters at most .")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Rating is required")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public double Rating { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }
+    }
+}
