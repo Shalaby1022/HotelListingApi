9bcc054c4555da34b2b4ae0284d57fe11e20d66c
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-12-04 01:20:02 +0200

    "Implement Hotel_List_Resource_Parameters class for flexible hotel listing filtering and pagination."

---
+++ HotelListingApi/Resource Parameters/Hotel_List Resource Parameters.cs
@@ -0,0 +1,19 @@
+﻿namespace HotelListingApi.HotelListResourceParameters
+{
+    public class Hotel_List_Resource_Parameters
+    {
+        const int MaxPageSize = 20;
+        public string? name { get; set; }
+        public string? SearchQuery { get; set; }
+
+        public int PageNumber = 1;
+
+        private int _pageSize = 10;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+    }
+}
