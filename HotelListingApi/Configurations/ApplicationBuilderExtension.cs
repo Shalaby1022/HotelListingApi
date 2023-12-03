c49383db18626de15f57805454f09c04f0974b52
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-12-04 01:24:28 +0200

    Create an extension method AddGlobalErrorHandlingMiddleWare for IApplicationBuilder to add global error handling middleware using GlobalExceptionHandlingMiddleWare in the HotelListingApi.Configurations namespace

---
+++ HotelListingApi/Configurations/ApplicationBuilderExtension.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace HotelListingApi.Configurations
+{
+    public static class ApplicationBuilderExtension
+    {
+        public static IApplicationBuilder  AddGlobalErrorHandlingMiddleWare(this IApplicationBuilder applicationBuilder) 
+            => applicationBuilder.UseMiddleware<GlobalExceptionHandlingMiddleWare>();
+    }
+}
