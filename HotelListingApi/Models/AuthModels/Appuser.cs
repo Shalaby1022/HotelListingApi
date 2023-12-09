30453d1fd5e5dca338f148e6ae1dd542066176a3
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-12-09 18:11:06 +0200

    extend IdentityUser with additional user details

---
+++ HotelListingApi/Models/AuthModels/Appuser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelListingApi.Models.AuthModels
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        public List<RefreshToken> RefreshTokens { get; set; }
+
+    }
+}
